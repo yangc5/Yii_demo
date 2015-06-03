5bab8e279fb7683ef69b614be8762ad633a8f0a8
cyang <cyang@trin-portal-u12.(none)>
2015-06-03 16:46:26 -0500

    Created first yii project

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'dblib:host=10.71.55.35;dbname=Chen_test',
+    'username' => 'thgdruser',
+    'password' => 'adrc123',
+    'charset' => 'utf8',
+
+];
+//   'db.connectionString' => 'dblib:host=10.71.55.35;dbname=Chen_test',
+//    'db.username' => 'thgdruser',
+//    'db.password' => 'adrc123',
+
