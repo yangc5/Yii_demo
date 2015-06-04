29b0700723c11047af1374ca0e2be843ba66e966
cyang <cyang@trin-portal-u12.(none)>
2015-06-04 15:40:12 -0500

    modify Country model file by generating from Gii

--- models/Country.php
+++ models/Country.php
@@ -2,8 +2,46 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Country extends ActiveRecord
+/**
+ * This is the model class for table "country".
+ *
+ * @property string $code
+ * @property string $name
+ * @property integer $population
+ */
+class Country extends \yii\db\ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'country';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['code', 'name'], 'string'],
+            [['population'], 'integer']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'code' => 'Code',
+            'name' => 'Name',
+            'population' => 'Population',
+        ];
+    }
 }
